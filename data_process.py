9aab3d7682a3f5af9208c18b1b1ed5bc9fb2fa0f
baohaoyuan <62395524+baohaoyuan@users.noreply.github.com>
2023-10-12 23:35:47 -0700

    Add files via upload

--- data_process.py
+++ data_process.py
@@ -22,21 +22,24 @@ col = ['Day','PreVisits', 'PPAgree', 'PostStorefront','ThankYou(Hit)','Verificat
 def data_prep(mycsv, col):
     visit = pd.read_csv(mycsv, skiprows = 15,names = col, delimiter = ',',index_col=False)  
     df = visit.iloc[:,:7]
-    df.iloc[[5,6]].round(2)
+    file_name = mycsv[0:-4]
+
     df['Pre'] = 1
     df['Agree/Pre'] = df['PPAgree']/df['PreVisits']
     df['Post/Agree'] = df['PostStorefront']/df['PPAgree']
     df['Orders/Post'] = df['ThankYou(Hit)']/df['PostStorefront']
+    cols = ['VerificationSuccessRate','ConversionRate','Pre','Agree/Pre','Post/Agree','Orders/Post']
+    df[cols] = df[cols].round(2)    
     df = df.transpose()
-    df.to_csv(mycsv[0:-4]+'_out.csv', encoding='utf-8', sep=',')
-    if os.path.exists(mycsv[0:-4]+'_out.csv'):
-        os.remove(mycsv[0:-4]+'_out.csv')
-        df.to_csv(mycsv[0:-4]+'_out.csv', encoding='utf-8', sep=',')
+    
+    if os.path.exists(file_name+'_out.csv'):
+        os.remove(file_name+'_out.csv')
+    df.to_csv(file_name+'_out.csv', encoding='utf-8', sep=',')
     '''
     try:
-        df.to_csv(mycsv[0:-4]+'_out.csv', encoding='utf-8', sep=',')
+        df.to_csv(file_name+'_out.csv', encoding='utf-8', sep=',')
     except FileExistsError:
-        df.to_csv(mycsv[0:-4]+'_out2.csv', encoding='utf-8', sep=',')
+        df.to_csv(file_name+'_out2.csv', encoding='utf-8', sep=',')
     '''
 
 if __name__ == "__main__":
